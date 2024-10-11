7a1bd8515b725e583e96fe1916d4a8994b1c3cee
FirdausyTama <atama.firdausy@gmail.com>
2024-10-12 00:52:38 +0700

    membuat data null agar tidak eror

--- app/src/main/java/com/example/pertemuansatu/NullSafety.kt
+++ app/src/main/java/com/example/pertemuansatu/NullSafety.kt
@@ -3,5 +3,12 @@ package com.example.pertemuansatu
 fun nullSafety() {
     // neverNull has String type
     var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null her e"
+    // This is OK
+    nullable = null
 }
 

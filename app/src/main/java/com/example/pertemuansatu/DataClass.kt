c82ba8fa57427079d41e5c311e677d7a6fbb8840
FirdausyTama <atama.firdausy@gmail.com>
2024-10-11 22:33:48 +0700

    Perbandingan mengecek apakah dua objek dengan email berbeda dianggap sama ternyata hasilnya nggak sama

--- app/src/main/java/com/example/pertemuansatu/DataClass.kt
+++ app/src/main/java/com/example/pertemuansatu/DataClass.kt
@@ -7,3 +7,12 @@ data class DataClass(
     val id: Int,
     var email: String
 )
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    //Fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+}

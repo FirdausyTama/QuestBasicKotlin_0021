36dbcf40ae8120d02ece67e2a981dbd1efdc6ae2
FirdausyTama <atama.firdausy@gmail.com>
2024-10-11 22:25:25 +0700

    membuat data class

---
+++ app/src/main/java/com/example/pertemuansatu/DataClass.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuansatu
+
+// Data Class
+// Data class adalah class yang digunakan untuk menyimpan data.
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)

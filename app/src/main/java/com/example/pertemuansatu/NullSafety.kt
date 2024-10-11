ee6f79b3a08f641164038d5a93b16591e75bea46
FirdausyTama <atama.firdausy@gmail.com>
2024-10-12 01:16:17 +0700

    tidak menerima nilai nol

--- app/src/main/java/com/example/pertemuansatu/NullSafety.kt
+++ app/src/main/java/com/example/pertemuansatu/NullSafety.kt
@@ -21,5 +21,15 @@ fun nullSafety() {
     // Safe call operator
     println(neverNull.length) // 18
     println(nullable?.length) // null
+
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+    println(strLength (neverNull)) // 18
+}
+
+fun main() {
+    nullSafety()
 }
 

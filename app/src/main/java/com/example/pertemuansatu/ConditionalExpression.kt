2b61937155dadb184ac641b711705314a354e6f0
FirdausyTama <atama.firdausy@gmail.com>
2024-10-12 13:22:40 +0700

    try-catch dalam fungsi ConditionalStatement untuk menangani konversi String ke Integer

--- app/src/main/java/com/example/pertemuansatu/ConditionalExpression.kt
+++ app/src/main/java/com/example/pertemuansatu/ConditionalExpression.kt
@@ -28,5 +28,12 @@ fun ConditionalStatement() {
         else -> println("Invalid Day")
     }
 
-
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
 }
